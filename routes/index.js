e10ce5e02ea6732156cae65aae755d40e08afe1f
Daniel <martin.da.1200@gmail.com>
2013-08-09 11:45:55 -0500

    initial commit; testing with node, express, backbone, mongo

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var db = require('../database.js');
+
+exports.index = function(req, res) {
+    res.render("index");
+};
+
+exports.books = {};
+
+exports.books.all = function(req, res) {
+    db.books.find(function(err, books) {
+	    if (err) return;
+	    res.json(books);
+	});
+};
+
+exports.books.one = function(req, res) {
+    var bookId = db.ObjectId(req.params.id);
+    db.books.findOne({"_id": bookId}, function (err, book) {
+	    if (err) return;
+	    res.json(book);
+	});
+};
+
+exports.books.create = function(req, res) {
+    res.json(req.body);
+    db.books.save(req.body);
+};
